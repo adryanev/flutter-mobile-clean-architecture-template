3d713ffcf0e1202a57341a6f0a8563d6d437b36a
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-08-14 09:01:15 +0700

    feat: add flash snackbar

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -6,49 +6,75 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:template/app/router/app_router.dart';
+import 'package:template/core/extensions/context_extensions.dart';
 import 'package:template/core/utils/constants.dart';
+import 'package:template/injector.dart';
 import 'package:template/l10n/l10n.dart';
+import 'package:template/shared/flash/presentation/blocs/cubit/flash_cubit.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(ScreenUtilSize.width, ScreenUtilSize.height),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp.router(
-          scaffoldMessengerKey: rootScaffoldMessengerKey,
-          theme: ThemeData(
-            appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-            colorScheme: ColorScheme.fromSwatch(
-              accentColor: const Color(0xFF13B9FF),
-            ),
-            fontFamily: GoogleFonts.poppins().fontFamily,
-            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<FlashCubit>(),
+        ),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<FlashCubit, FlashState>(
+            listener: (context, state) {
+              state.when(
+                disappeared: () => null,
+                appeared: (message) => context.showSnackbar(
+                  message: message,
+                ),
+              );
+            },
           ),
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-          ],
-          supportedLocales: AppLocalizations.supportedLocales,
-          routeInformationParser: appRouter.routeInformationParser,
-          routerDelegate: appRouter.routerDelegate,
-          routeInformationProvider: appRouter.routeInformationProvider,
-          builder: (context, widget) {
-            return MediaQuery(
-              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.sp),
-              child: widget!,
+        ],
+        child: ScreenUtilInit(
+          designSize: const Size(ScreenUtilSize.width, ScreenUtilSize.height),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp.router(
+              scaffoldMessengerKey: rootScaffoldMessengerKey,
+              theme: ThemeData(
+                appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+                colorScheme: ColorScheme.fromSwatch(
+                  accentColor: const Color(0xFF13B9FF),
+                ),
+                fontFamily: GoogleFonts.poppins().fontFamily,
+                textTheme:
+                    Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+              ),
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+              ],
+              supportedLocales: AppLocalizations.supportedLocales,
+              routeInformationParser: appRouter.routeInformationParser,
+              routerDelegate: appRouter.routerDelegate,
+              routeInformationProvider: appRouter.routeInformationProvider,
+              builder: (context, widget) {
+                return MediaQuery(
+                  data: MediaQuery.of(context).copyWith(textScaleFactor: 1.sp),
+                  child: widget!,
+                );
+              },
             );
           },
-        );
-      },
+        ),
+      ),
     );
   }
 }
--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:template/core/utils/constants.dart';
+import 'package:template/shared/flash/presentation/blocs/cubit/flash_cubit.dart';
 
 extension BuildContextX on BuildContext {
-  // void displayFlash(String message) {
-  //   read<FlashCubit>().displayFlash(message);
-  // }
+  void displayFlash(String message) {
+    read<FlashCubit>().displayFlash(message);
+  }
 
   void showSnackbar({
     required String message,
